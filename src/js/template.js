19c268114f5125d9280d038ad88cb310ca31315f
Marcus Paulo Oliveira <47436367+MarcusPOliveira@users.noreply.github.com>
2024-04-15 18:20:39 -0300

    feat: template component

--- src/js/template.js
+++ src/js/template.js
@@ -1,3 +1,96 @@
+class TemplateHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <div class="header-container">
+          <nav class="menu">
+            <div class="toggle-btn">
+              <i class="fa-solid fa-bars"></i>
+            </div>
+            <div class="left-side">
+              <a href="/src/index.html">
+                <img class="logo" src="/src/assets/logo.png" alt="Logo" />
+              </a>
+              <ul>
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Alimentos</a></li>
+                <li><a href="#">Rótulos</a></li>
+                <li><a href="#">Profissionais de saúde</a></li>
+                <li><a href="#">Mercados</a></li>
+                <li><a href="#">Acesso Admin</a></li>
+              </ul>
+            </div>
+            <button class="admin-button">Acesso Admin</button>
+          </nav>
+
+          <div class="dropdown-menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Alimentos</a></li>
+            <li><a href="#">Rótulos</a></li>
+            <li><a href="#">Profissionais de saúde</a></li>
+            <li><a href="#">Mercados</a></li>
+            <li><a href="#">Acesso Admin</a></li>
+          </div>
+        </div>
+      </header>
+    `;
+  }
+}
+
+customElements.define('template-header', TemplateHeader);
+
+class TemplateFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <footer>
+        <div class="footer-container">
+          <img class="logo" src="/src/assets/logo.png" alt="Logo" />
+          <div class="separator"></div>
+          <div class="footer-links">
+            <div>
+              <p>Alimentos</p>
+              <ul>
+                <li><a href="#">Como ler os rótulos</a></li>
+                <li><a href="#">Todos os alimentos</a></li>
+                <li><a href="#">Categorias de alimentos</a></li>
+                <li>
+                  <a href="/src/pages/ranking-alimentos.html">
+                    Ranking de Alimentos
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p>Profissionais de saúde</p>
+              <ul>
+                <li><a href="#">Como ler os rótulos</a></li>
+                <li><a href="#">Todos os alimentos</a></li>
+                <li><a href="#">Categorias de alimentos</a></li>
+                <li><a href="#">Restrição alimentar</a></li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Mercados</a></li>
+                <li><a href="#">Notícias</a></li>
+                <li><a href="#">Sugestões</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="separator"></div>
+          <div class="copyright">
+            <p>© Copyright. Rótulo Certo 2024</p>
+          </div>
+        </div>
+      </footer>
+    `;
+  }
+}
+
+customElements.define('template-footer', TemplateFooter);
+
+
 const toggleBtn = document.querySelector('.toggle-btn');
 const toggleBtnIcon = document.querySelector('.toggle-btn i');
 const dropdownMenu = document.querySelector('.dropdown-menu');
@@ -17,3 +110,4 @@ document.addEventListener('click', (event) => {
     toggleBtnIcon.classList = 'fa-solid fa-bars';
   }
 });
+
