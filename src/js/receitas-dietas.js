6d11c0dd4961c9715fdba3a8c3f73cc0cd6cfa0e
Italo Penaforte <italopenaforte@gmail.com>
2024-05-19 15:43:18 -0300

    adiciona busca por objetivo de receita

--- src/js/receitas-dietas.js
+++ src/js/receitas-dietas.js
@@ -4,18 +4,18 @@ let paginationInfo = document.getElementById('pagination-info')
 let searchInput = document.getElementById('searchInput')
 let searchButon = document.getElementById('searchButon')
 
-const createTableData = (product) => {
+const gerarReceitas = (receita) => {
     let div = document.createElement('div')
     div.innerHTML = `
     <div class="info">
-        <div class="img"><img class="img" src="${product.imagem}"></div>
+        <div class="img"><img class="img" src="${receita.imagem}"></div>
         <div class="text">
-        <h3>${product.titulo}</h3>
+        <h3>${receita.titulo}</h3>
         <div>
             <p>
-            ${product.descricao}
+            ${receita.descricao}
             </p>
-            <p>Ingredientes: ${product.ingredientes.join(", ")}</span></p>
+            <p>Ingredientes: ${receita.ingredientes.join(", ")}</span></p>
         </div>
         </div>
     </div>
@@ -23,14 +23,14 @@ const createTableData = (product) => {
     return div
 }
 
-const renderTablePage = (data) => {
+const renderizarReceitas = (data) => {
     const startIndex = (data.page - 1) * data.itemsPerPage
     const endIndex = startIndex + data.itemsPerPage
     const receitas = data.recipes.slice(startIndex, endIndex)
     infos.innerHTML = ''
-    receitas.forEach((product) => {
-        console.log(product)
-        infos.appendChild(createTableData(product))
+    receitas.forEach((receita) => {
+        console.log(receita)
+        infos.appendChild(gerarReceitas(receita))
     })
     paginationInfo.textContent = `Página ${data.page} de ${data.totalPages}`
 }
@@ -39,30 +39,53 @@ fetch('../api/db.json').then((res) => {
     return res.json()
 }).then(({ receitas }) => {
     console.log('receitas', receitas)
-    renderTablePage(receitas)
+    renderizarReceitas(receitas)
 
     const pesquisarReceitas = () => {
         const searchTerm = searchInput.value.toLowerCase();
-        const filteredRecipes = receitas.recipes.filter((product) =>
-            product.titulo.toLowerCase().includes(searchTerm)
+        const filteredRecipes = receitas.recipes.filter((receita) =>
+            receita.titulo.toLowerCase().includes(searchTerm)
         );
 
-        renderTablePage({ ...receitas, recipes: filteredRecipes, page: 1 });
+        renderizarReceitas({ ...receitas, recipes: filteredRecipes, page: 1 });
     };
 
+    // search by input text
     searchButon.addEventListener('click', pesquisarReceitas);
 
+    let searchPerdaGordura = document.getElementById('perda-gordura')
+    let searchGanhoMassa = document.getElementById('ganho-massa')
+    let searchVegana = document.getElementById('vegana')
+    let searchVegetariana = document.getElementById('vegetariana')
+
+
+    const filterByObjective = (objective) => {
+        console.log('objective', objective)
+        const filteredRecipes = receitas.recipes.filter((receita) =>
+            receita.objetivo.filter((obj) => obj == objective).length > 0
+        );
+
+        renderizarReceitas({ ...receitas, recipes: filteredRecipes, page: 1 });
+    };
+
+    searchPerdaGordura.addEventListener('click', () => filterByObjective('Perda de Gordura'))
+    searchGanhoMassa.addEventListener('click', () => filterByObjective('Ganho de Massa'))
+    searchVegana.addEventListener('click', () => filterByObjective('Vegana'))
+    searchVegetariana.addEventListener('click', () => filterByObjective('Vegetariana'))
+
+    // previous page
     document.getElementById('prev-page').addEventListener('click', () => {
         if (receitas.page > 1) {
             receitas.page--
-            renderTablePage(receitas)
+            renderizarReceitas(receitas)
         }
     })
 
+    // next page
     document.getElementById('next-page').addEventListener('click', () => {
         if (receitas.page < receitas.totalPages) {
             receitas.page++
-            renderTablePage(receitas)
+            renderizarReceitas(receitas)
         }
     })
 })
