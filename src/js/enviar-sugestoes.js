59dab14e301ccdba7b6ec809b4a403da1f1a1480
Raul Duarte <raultorresoliveira@gmail.com>
2024-06-02 19:51:57 -0300

    adicionar sugestoes via local storage

--- src/js/enviar-sugestoes.js
+++ src/js/enviar-sugestoes.js
@@ -1,13 +1,13 @@
-let sugestoes;
+let sugestoes = { suggestionsList: [] };
 
-fetch('../api/db.json')
-    .then((res) => res.json())
-    .then(({ sugestoes: sugestoesData }) => {
-        sugestoes = sugestoesData;
-        console.log('sugestoes', sugestoes);
-    });
 
 document.addEventListener('DOMContentLoaded', function() {
+    var storedSugestoes = localStorage.getItem('sugestoes');
+    if (storedSugestoes) {
+        sugestoes = JSON.parse(storedSugestoes);
+    }
+    console.log('Sugestões carregadas:', sugestoes);
+
     var form = document.getElementById('form-sugestoes');
 
     form.addEventListener('submit', function(event) {
@@ -25,31 +25,13 @@ document.addEventListener('DOMContentLoaded', function() {
         sugestoes.suggestionsList.push(sugestObj);
 
         var updatedSugestoesJSON = {
-            ...sugestoes, 
-            "suggestionsList": sugestoes.suggestionsList 
+            ...sugestoes,
+            "suggestionsList": sugestoes.suggestionsList
         };
 
         console.log('Sugestões atualizadas:', updatedSugestoesJSON);
 
-        fetch('../api/db.json', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(updatedSugestoesJSON)
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao atualizar sugestões');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Resposta do servidor:', data);
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-        });
+        localStorage.setItem('sugestoes', JSON.stringify(updatedSugestoesJSON));
 
         form.reset();
     });
